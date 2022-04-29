9cd9a2ad2adb0f8b2214b20e4443ae6d6e367a72
jordimiyai <jgalvan89@gmail.com>
2022-04-29 18:29:13 -0300

    changes in  CORS config

--- thumbnail-generator-api/src/app.js
+++ thumbnail-generator-api/src/app.js
@@ -7,9 +7,9 @@ server.name = 'API';
 
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+    res.header('Access-Control-Allow-Origin', '*'); 
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
   });
